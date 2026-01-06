a864e2bf1d4a1fdea3d152ad4e4c82147f50a7d0
Joby Elliott <code@byjoby.com>
2026-01-06 23:14:34 +0000

    more aggressive caching

--- html/index.php
+++ html/index.php
@@ -9,7 +9,7 @@ $file = fopen(__DIR__ . '/links.csv', 'r');
 if ($file === false) {
     // problem opening file, fall out into 500
     header("Content-Type: text/plain; charset=utf-8");
-    header("Cache-Control: public, max-age=300, s-maxage=300");
+    header("Cache-Control: public, max-age=300, s-maxage=300, stale-while-revalidate=86400, stale-if-error=604800");
     http_response_code(500);
     echo 'Internal server error';
     return;
@@ -18,7 +18,7 @@ if ($file === false) {
 // if slug is empty, show list of all links
 if ($slug == '') {
     header("Content-Type: text/html; charset=utf-8");
-    header("Cache-Control: public, max-age=300, s-maxage=300");
+    header("Cache-Control: public, max-age=300, s-maxage=300, stale-while-revalidate=86400, stale-if-error=604800");
     include 'home.inc';
     return;
 }
@@ -29,7 +29,7 @@ while (($line = fgets($file)) !== false) {
     $line = array_map(trim(...), $line);
     if ($slug == $line[0]) {
         // finish up and bounce if there's a hit
-        header("Cache-Control: public, max-age=3600, s-maxage=3600");
+        header("Cache-Control: public, max-age=86400, s-maxage=86400, stale-while-revalidate=86400, stale-if-error=604800");
         header('Location: ' . $line[1], true, 301);
         return;
     }
@@ -37,6 +37,6 @@ while (($line = fgets($file)) !== false) {
 
 // otherwise fall out into a 404
 header("Content-Type: text/plain; charset=utf-8");
-header("Cache-Control: public, max-age=300, s-maxage=300");
+header("Cache-Control: public, max-age=300, s-maxage=300, stale-while-revalidate=86400, stale-if-error=604800");
 http_response_code(404);
 echo 'Link not found';
